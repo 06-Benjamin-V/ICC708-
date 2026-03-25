0a601f8ec44288ba6df34fecfd8e358ad77ca403
06-Benjamin-V <b.valenzuela06@ufromail.cl>
2026-03-25 12:43:51 -0300

    pequeño cambio

--- src/main/java/com/example/EjercicioA.java
+++ src/main/java/com/example/EjercicioA.java
@@ -7,7 +7,6 @@ public class EjercicioA {
 
     public static Map<Integer, Integer> contarL(int[] arr) {
         Map<Integer, Integer> result = new HashMap<>();
-        long iTiempo = System.nanoTime();
         for (int i = 0; i < arr.length; i++) {
             int count = 0;
             for (int j = 0; j < arr.length; j++) {
@@ -17,20 +16,15 @@ public class EjercicioA {
             }
             result.put(arr[i], count);
         }
-        long fTiempo = System.nanoTime();
-        System.out.println("tiempo lineal: " + (fTiempo - iTiempo) / 1000000.0 + " ms");
 
         return result;
     }
 
     public static Map<Integer, Integer> contarR(int[] arr) {
         Map<Integer, Integer> result = new HashMap<>();
-        long iTiempo = System.nanoTime();
         for (int num : arr) {
             result.put(num, result.getOrDefault(num, 0) + 1);
         }
-        long fTiempo = System.nanoTime();
-        System.out.println("tiempo hash: " + (fTiempo - iTiempo) / 1000000.0 + " ms");
         return result;
     }
 }
